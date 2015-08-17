a76b0c4f6e731895d9690a9a336acc07f2103081
Shokunin San <me@flatmap.io>
2015-08-17 09:00:52 +0200

    Added p05.

---
+++ p05.scala
@@ -0,0 +1,16 @@
+// Builtin
+def reverseBuiltin[A](l: List[A]): List[A] = l.reverse
+
+// Tail recursive version
+import annotation._
+def reverseRecursive[A](l: List[A]): List[A] = {
+  @tailrec
+  def _do(l1: List[A], r: List[A]): List[A] = l1 match {
+    case Nil => r
+    case x :: xs => _do(xs, x :: r)
+  }
+  _do(l, Nil)
+}
+
+// Fold
+def reverseFold[A](l: List[A]): List[A] = ???
