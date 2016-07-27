8202a3579d5c3845dd019613a09988da7fed7a2c
Shokunin San <me@flatmap.io>
2016-07-27 23:19:28 +0200

    Added p06 and restructured project.

---
+++ src/main/scala/p06.scala
@@ -0,0 +1,12 @@
+/**
+
+P06 (*) Find out whether a list is a palindrome.
+
+Example:
+
+scala> isPalindrome(List(1, 2, 3, 2, 1))
+res0: Boolean = true
+
+*/
+
+def isPalindrome(xs: List[Int]): Boolean = xs == xs.reverse 
