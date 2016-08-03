9deab01bd82bfee8704cdc18994f9ae03ef4d799
Shokunin San <me@flatmap.io>
2016-08-03 22:29:38 +0200

    Added recursive versions of P08.

--- src/main/scala/P08.scala
+++ src/main/scala/P08.scala
@@ -10,26 +10,38 @@ scala> compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
 res0: List[Symbol] = List('a, 'b, 'c, 'a, 'd, 'e)
 
 */
-trait P08 {
+import annotation._
+
+object P08 {
 
   def compress[T](xs: List[T]): List[T] = {
-
     var last = xs.head
     var l = List(last)
-
     for (i <- xs.tail) {
       if (i != last) {
         l = i :: l
         last = i
       }
     }
-
     l.reverse
   }
 
   def compressRecursive[T](xs: List[T]): List[T] = xs match {
-    case x :: xs if Some(x) != xs.headOption => x :: compressRecursive(xs)
-    case x :: xs => compressRecursive(xs)
+    case x :: xs =>
+      if(xs.headOption == Some(x)) compressRecursive(xs)
+      else x :: compressRecursive(xs)
+    case _ => Nil
+  }
+
+  def compressTailRecursive[T](l: List[T]): List[T] = {
+    @tailrec
+    def _do(xs: List[T], acc: List[T]): List[T] = xs match {
+      case x :: xs =>
+        if(xs.headOption == Some(x)) _do(xs, acc)
+        else _do(xs, x :: acc)
+      case _ => acc.reverse
+    }
+    _do(l, Nil)
   }
 
 }
--- src/test/scala/P08Spec.scala
+++ src/test/scala/P08Spec.scala
@@ -1,6 +1,7 @@
 import org.scalatest._
+import P08._
 
-class P08Spec extends FlatSpec with Matchers with P08 {
+class P08Spec extends FlatSpec with Matchers {
 
   "compress" should "eliminate consecutive duplicates of list elements" in {
     compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should equal (List('a, 'b, 'c, 'a, 'd, 'e))
@@ -8,6 +9,10 @@ class P08Spec extends FlatSpec with Matchers with P08 {
 
   "compressRecursive" should "eliminate consecutive duplicates of list elements" in {
     compressRecursive(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should equal (List('a, 'b, 'c, 'a, 'd, 'e))
+  }
+
+  "compressTailRecursive" should "eliminate consecutive duplicates of list elements" in {
+    compressTailRecursive(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should equal (List('a, 'b, 'c, 'a, 'd, 'e))
   }
 
 }
