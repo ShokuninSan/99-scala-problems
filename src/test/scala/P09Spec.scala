a976da91ac79d5d8af1516d16adc501ac8a803ba
Shokunin San <me@flatmap.io>
2016-08-03 22:53:40 +0200

    Added test and P09 stub.

---
+++ src/test/scala/P09Spec.scala
@@ -0,0 +1,10 @@
+import org.scalatest._
+import P09._
+
+class P09Spec extends FlatSpec with Matchers {
+
+  "pack" should "place repeated elements into sublists" in {
+    pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should equal (List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e)))
+  }
+
+}
