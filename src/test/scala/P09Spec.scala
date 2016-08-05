930ed09855612e5ce7d5ba6043007f64ed53c41e
Shokunin San <me@flatmap.io>
2016-08-05 23:51:35 +0200

    Added test for P09.

--- src/test/scala/P09Spec.scala
+++ src/test/scala/P09Spec.scala
@@ -7,4 +7,8 @@ class P09Spec extends FlatSpec with Matchers {
     pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should equal (List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e)))
   }
 
+  "pack" should "return List(Nil) with Nil as parameter" in {
+    pack(Nil) should equal (List(Nil))
+  }
+
 }
