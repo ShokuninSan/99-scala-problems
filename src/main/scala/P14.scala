7fd86e82956c269bd27ae86b24efb53cf97aa121
Shokunin San <me@flatmap.io>
2016-08-06 16:39:12 +0200

    Added P14.

---
+++ src/main/scala/P14.scala
@@ -0,0 +1,18 @@
+/**
+P14 (*) Duplicate the elements of a list.
+
+Example:
+
+scala> duplicate(List('a, 'b, 'c, 'c, 'd))
+res0: List[Symbol] = List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd)
+*/
+object P14 {
+
+  def duplicate[T](xs: List[T]): List[T] = xs match {
+    case x :: xs => x :: x :: duplicate(xs)
+    case _ => Nil
+  }
+
+  def duplicateFolded[T](xs: List[T]): List[T] = xs.foldRight(List[T]())((x,acc) => x :: x :: acc)
+
+}
---
+++ src/test/scala/P14Spec.scala
@@ -0,0 +1,14 @@
+import org.scalatest._
+import P14._
+
+class P14Spec extends FlatSpec with Matchers {
+
+  "duplicate" should "duplicate the elements of a list" in {
+    duplicate(List('a, 'b, 'c, 'c, 'd)) should equal (List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd))
+  }
+
+  "duplicateFolded" should "duplicate the elements of a list" in {
+    duplicateFolded(List('a, 'b, 'c, 'c, 'd)) should equal (List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd))
+  }
+
+}
