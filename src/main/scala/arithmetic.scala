93ea1b7b23f1c5f63ae01718a4d34a487d98feb9
Shokunin San <me@flatmap.io>
2016-08-07 16:38:40 +0200

    Added descriptions.

--- src/main/scala/arithmetic.scala
+++ src/main/scala/arithmetic.scala
@@ -7,9 +7,20 @@ package arithmetic {
 
     import S99Int._
 
+    /**
+     * P31 (**) Determine whether a given integer number is prime.
+     * scala> 7.isPrime
+     * res0: Boolean = true
+     */
     def isPrime: Boolean =
       List.range(start-1, 1, -1).filter(i => (start % i) == 0).isEmpty
 
+    /**
+     * P33 (*) Determine whether two positive integer numbers are coprime.
+     * Two numbers are coprime if their greatest common divisor equals 1.
+     * scala> 35.isCoprimeTo(64)
+     * res0: Boolean = true
+     */
     def isCoprimeTo(r: Int): Boolean = gcd(start, r) == 1
 
   }
@@ -17,6 +28,12 @@ package arithmetic {
   object S99Int {
     implicit def int2S99Int(i: Int): S99Int = new S99Int(i)
 
+    /**
+     * P32 (**) Determine the greatest common divisor of two positive integer  * numbers.
+     * Use Euclid's algorithm.
+     * scala> gcd(36, 63)
+     * res0: Int = 9
+     */
     @tailrec
     def gcd(a: Int, b: Int): Int =
       if (b == 0) a
