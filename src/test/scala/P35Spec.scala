e9be5152fbf1d48cbec805da2f2b402203cc6727
Shokunin San <me@flatmap.io>
2016-08-09 19:29:14 +0200

    Updated test for P35.

--- src/test/scala/P35Spec.scala
+++ src/test/scala/P35Spec.scala
@@ -4,7 +4,11 @@ import arithmetic.S99Int._
 class P35Spec extends FlatSpec with Matchers {
 
   "primeFactors" should "determine the prime factors of a given positive integer" in {
-    315.primeFactors should equal (List(3, 3, 5, 7))
+    for ((value, expectedFactors) <- List(
+      (315, List(3, 3, 5, 7)),
+      (105, List(3, 5, 7)),
+      (1049, List(1049))))
+      value.primeFactors should equal (expectedFactors)
   }
 
 }
